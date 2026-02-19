de45a415d4345a78ecbc5f243bebfc1664df092f
punpunie <punpunie@proton.me>
2026-02-19 19:20:36 +0100

    fixed thingy

--- src/main/java/io/github/punpunie/suwa/SuwaConfig.java
+++ src/main/java/io/github/punpunie/suwa/SuwaConfig.java
@@ -15,7 +15,7 @@ public class SuwaConfig extends BaseConfig {
 	static Predicate<Float> isBetween180 = val -> val >= 0 && val <= 180;
 	static Predicate<Float> isBetweenm5v5 = val -> val >= -5 && val <= 5;
 
-	public static final FloatOption fovValue = new FloatOption("FOV", "Field of View", 40F, isBetween180);
+	public static final FloatOption fovValue = new FloatOption("FOV", "Field of View", 70F, isBetween180);
 	public static final FloatOption viewmodelX = new FloatOption("viewX", "Viewmodel X", 0F, isBetweenm5v5);
 	public static final FloatOption viewmodelY = new FloatOption("viewY", "Viewmodel Y", 0F, isBetweenm5v5);
 	public static final FloatOption viewmodelZ = new FloatOption("viewZ", "Viewmodel Z", 0F, isBetweenm5v5);
